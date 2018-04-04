c86884c84d999088171740a7c377cfe135e484bf
Gerson Cardoso Filho <gersoncfilho@mac.com>
2018-04-04 08:56:26 -0300

    HomeController
    Limpeza de comentários

--- BaltaStore.Api/Controllers/HomeController.cs
+++ BaltaStore.Api/Controllers/HomeController.cs
@@ -7,9 +7,6 @@ namespace BaltaStore.Api.Controllers
     {
         [HttpGet]
         [Route("")]
-        // [Route("clientes")] // Listar todos os clientes
-        // [Route("clientes/2587")] // Listar o cliente 2587
-        // [Route("clientes/2587/pedidos")] // Pedidos do cliente 2587
         public object Get()
         {
             return new { version = "Version 0.0.2" };
